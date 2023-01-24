e81321b430533cba913f9a310f057c4694a17a05
gfrnca <gabrielbfranca27@gmail.com>
2023-01-24 16:56:28 -0300

    Redes sociais no footer

--- src/components/organisms/Footer/Footer.styled.ts
+++ src/components/organisms/Footer/Footer.styled.ts
@@ -2,14 +2,45 @@ import styled from 'styled-components'
 
 export const FooterStyles = styled.div `
   width: 100%;
-  height: 90px;
+  padding: 40px;
   background: #050012;
+
+  .container {
+    width: 100%;
+    height: 100%;
+  }
 
   .footer-wrapper {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 100%;
     height: 100%;
+
+    .social-medias {
+      margin-top: 20px;
+      display: flex;
+      gap: 40px;
+    }
+
+    .social-medias .social {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 8px;
+      width: 35px;
+      height: 35px;
+      background: #0a0026;
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      border-radius: 100%;
+      cursor: pointer;
+      transition: 0.2s all;
+
+      &:hover {
+        scale: 1.22;
+        background: #0f0038;
+      }
+    }
   }
 `
--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,23 +2,28 @@
 import { FooterStyles } from './Footer.styled'
 
 /* Font Awesome */
-
+import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Container } from '../../../styles/global'
 
 const Footer = () => {
   return (
     <FooterStyles>
-      <div className="footer-wrapper">
-        <p>Desenvolvido com ❤️ pela RX6</p>
+      <Container className='container'>
+        <div className="footer-wrapper">
+          <p>Desenvolvido com ❤️ pela RX6</p>
 
-        <div className="social-medias">
-          <div className="social">
-            
-          </div>
-          <div className="social">
-
+          <div className="social-medias">
+            <div className="social">
+              <FontAwesomeIcon icon={ faInstagram } />
+            </div>
+            <div className="social">
+              <FontAwesomeIcon icon={ faDiscord } />
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     </FooterStyles>
   )
 }
